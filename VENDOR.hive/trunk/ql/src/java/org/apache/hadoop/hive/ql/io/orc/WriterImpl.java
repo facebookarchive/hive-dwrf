47edfe9eca0c718968662fc4be3afcc58e06715f
Kevin Wilfong <kevinwilfong@fb.com>
2013-05-24 20:38:19 +0000

    Fix bug in ORC with stripe size >= 1Gb
    Summary:
    If 2 * <stripe_size> is outside the range of ints, ORC uses Integer.MAX_VALUE as the block size when creating a file.
    
    However, Integer.MAX_VALUE isn't divisible by the bytes per  checksum HDFS uses (512 by default) so it throws an exception
    java.io.IOException: io.bytes.per.checksum(512) and blockSize(2147483647) do not match. blockSize should be a multiple of io.bytes.per.checksum
    
    To fix this, take the largest value divisible by the bytes per checksum which is less than Integer.MAX_VALUE as the upper bound.
    
    Test Plan:
    Ran a test case using 1Gb stripe size on our cluster and verified it works.
    
    Apparently, open source HDFS doesn't have the same check, so I can't make a qfile test for this.
    
    Running ptests.

--- VENDOR.hive/trunk/ql/src/java/org/apache/hadoop/hive/ql/io/orc/WriterImpl.java
+++ VENDOR.hive/trunk/ql/src/java/org/apache/hadoop/hive/ql/io/orc/WriterImpl.java
@@ -117,6 +117,7 @@ class WriterImpl implements Writer, MemoryManager.Callback {
   private final boolean buildIndex;
   private final MemoryManager memoryManager;
   private final boolean useVInts;
+  private final int dfsBytesPerChecksum;
 
   private final Configuration conf;
 
@@ -142,6 +143,7 @@ class WriterImpl implements Writer, MemoryManager.Callback {
     useVInts = conf.getBoolean(HiveConf.ConfVars.HIVE_ORC_USE_VINTS.varname,
         HiveConf.ConfVars.HIVE_ORC_USE_VINTS.defaultBoolVal);
     treeWriter = createTreeWriter(inspector, streamFactory, false, conf, useVInts);
+    dfsBytesPerChecksum = conf.getInt("io.bytes.per.checksum", 512);
     if (buildIndex && rowIndexStride < MIN_ROW_INDEX_STRIDE) {
       throw new IllegalArgumentException("Row stride must be at least " +
           MIN_ROW_INDEX_STRIDE);
@@ -1659,7 +1661,9 @@ class WriterImpl implements Writer, MemoryManager.Callback {
     if (rawWriter == null) {
       rawWriter = fs.create(path, false, HDFS_BUFFER_SIZE,
         fs.getDefaultReplication(),
-          Math.min(stripeSize * 2L, Integer.MAX_VALUE));
+          // Use the largest value less than or equal to Integer.MAX_VALUE which is divisible
+          // by dfsBytesPerChecksum as an upper bound (otherwise HDFS will throw an exception)
+          Math.min(stripeSize * 2L, (Integer.MAX_VALUE / dfsBytesPerChecksum) * dfsBytesPerChecksum));
       rawWriter.writeBytes(OrcFile.MAGIC);
       headerLength = rawWriter.getPos();
       writer = new OutStream("metadata", bufferSize, codec,
