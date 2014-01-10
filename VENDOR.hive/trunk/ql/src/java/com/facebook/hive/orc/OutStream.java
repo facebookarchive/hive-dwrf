e2d7f9b0f6aa57ea2ef0ee249bfef52c8132d3b3
Kevin Wilfong <kevinwilfong@fb.com>
2014-01-10 12:05:18 -0800

    Add license and copyright headers

--- VENDOR.hive/trunk/ql/src/java/com/facebook/hive/orc/OutStream.java
+++ VENDOR.hive/trunk/ql/src/java/com/facebook/hive/orc/OutStream.java
@@ -1,3 +1,5 @@
+//  Copyright (c) 2013, Facebook, Inc.  All rights reserved.
+
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
--- VENDOR.hive/trunk/ql/src/java/org/apache/hadoop/hive/ql/io/slice/BasicSliceOutput.java
+++ VENDOR.hive/trunk/ql/src/java/org/apache/hadoop/hive/ql/io/slice/BasicSliceOutput.java
@@ -12,7 +12,10 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations
  * under the License.
- */
+*/
+
+//  Copyright (c) 2013, Facebook, Inc.  All rights reserved.
+
 package org.apache.hadoop.hive.ql.io.slice;
 
 import java.io.IOException;
